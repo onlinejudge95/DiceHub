3009eda034384708e76e52db660ae7e0ee0b0ccd
onlinejudge95 <onlinejudge95@gmail.com>
2024-06-20 23:15:24 +0530

    Initializes shadcn

--- frontend/prettier.config.mjs
+++ frontend/prettier.config.mjs
@@ -1,8 +1,8 @@
 const config = {
-    trailingComma: 'es5',
-    tabWidth: 4,
-    semi: true,
-    singleQuote: true,
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
 };
 
 export default config;
