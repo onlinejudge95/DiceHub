96b74e4158678d38596d7f2ec9f328dbd3c034b1
onlinejudge95 <onlinejudge95@gmail.com>
2024-06-23 15:00:57 +0530

    Updates script to update the github action comment on new commits

--- .github/workflows/scripts/updatePr.js
+++ .github/workflows/scripts/updatePr.js
@@ -1,8 +1,22 @@
 module.exports = async ({ github, context }) => {
-  await github.rest.issues.createComment({
-    issue_number: context.issue.number,
+  const commentsResponse = await github.rest.issues.listComments({
     owner: context.repo.owner,
     repo: context.repo.repo,
-    body: `🚀 Preview URL: ${process.env.DEPLOYMENT_URL}`,
+    issue_number: context.issue.number,
   });
+
+  for (let comment of commentsResponse.body) {
+    if (
+      comment.user.type === "bot" &&
+      comment.user.login === "github-actions"
+    ) {
+      await github.rest.issues.createComment({
+        issue_number: context.issue.number,
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        body: `🚀 Preview URL: ${process.env.DEPLOYMENT_URL}`,
+      });
+      break;
+    }
+  }
 };
