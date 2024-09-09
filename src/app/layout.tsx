2d2780b9880acfcc866f9a47509e16745446aebd
Mayank Pathak <onlinejudge95@gmail.com>
2024-09-10 00:57:39 +0530

    Consumes postgres from dev env for development

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,13 +32,13 @@ export default function RootLayout({
       <head>
         {(process.env.NODE_ENV === 'development' ||
           process.env.VERCEL_ENV === 'preview') && (
-            // eslint-disable-next-line @next/next/no-sync-scripts
-            <script
-              data-project-id="IXh10dtmz7uN4NOkDMSf0RKn9P8QnDCfTOb0fhJB"
-              data-is-production-environment="false"
-              src="https://snippet.meticulous.ai/v1/meticulous.js"
-            />
-          )}
+          // eslint-disable-next-line @next/next/no-sync-scripts
+          <script
+            data-project-id="IXh10dtmz7uN4NOkDMSf0RKn9P8QnDCfTOb0fhJB"
+            data-is-production-environment="false"
+            src="https://snippet.meticulous.ai/v1/meticulous.js"
+          />
+        )}
       </head>
       <body className={ibm_plex_sans.variable + ' ' + gabarito.variable}>
         <SessionProviderWrapper>
